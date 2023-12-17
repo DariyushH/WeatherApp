f924f75c9fb442f65f543a9af58cd2e73245fe93
kaerimashou <reajlcade96@gmail.com>
2023-12-17 17:28:08 +0300

    Changed URIBuilder

--- WeatherApp/src/main/java/com/example/weatherapp/WeatherApi.java
+++ WeatherApp/src/main/java/com/example/weatherapp/WeatherApi.java
@@ -1,10 +1,10 @@
 package com.example.weatherapp;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriBuilder;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -24,10 +24,11 @@ public class WeatherApi {
         UriComponents uri = UriComponentsBuilder.newInstance()
                 .scheme("http")
                 .host("www.api.weatherapi.com")
-                .path("/v1/")
-                .query("current.json?key={apiKey}&q=Grodno&aqi=no")
-                .buildAndExpand(apiKey);
-        assertEquals("http://api.weatherapi.com/v1/current.json?key=\"+apiKey+\"&q=Grodno&aqi=no", uri.toUriString());
+                .path("/v1/current.json")
+                .queryParam("key", apiKey)
+                .queryParam("q", "Grodno")
+                .queryParam("aqi", "no")
+                .build();
         return restTemplate.getForObject(uri.toUriString(), String.class); //TODO Сделать получение в Java Class
     }
 
